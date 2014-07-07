64274cc04107ed5afeed3d62658250a85b3d3e12
Paul Grigoras <paul.grigoras90@gmail.com>
2014-07-07 08:43:08 -0700

    Separate Pawn methods.

--- Pawn.cpp
+++ Pawn.cpp
@@ -1,1 +1,21 @@
 #include "Pawn.hpp"
+
+std::vector<std::pair<int, int> > Pawn::validMoves() {
+  std::vector<std::pair<int, int> > pos;
+
+  // TODO check if squares are occupied
+  // first move can jump 2
+  if (color == WHITE) {
+    if (y == 1)
+      pos.push_back(std::make_pair(x, y + 2));
+    pos.push_back(std::make_pair(x, y + 1));
+  } else {
+    if (y == 6)
+      pos.push_back(std::make_pair(x, y - 2));
+    pos.push_back(std::make_pair(x, y - 1));
+  }
+
+  // TODO add diagonal capture movement
+  std::cout << "Pos: " << pos.size() << std::endl;
+  return pos;
+}
--- Pawn.hpp
+++ Pawn.hpp
@@ -9,36 +9,12 @@ class Pawn : public Piece {
 
 public:
   Pawn(int color, int x, int y) :
-    Piece(color, x, y, "black_pawn.bmp", "white_pawn.bmp") {
-    firstMove = true;
-  }
-
-  std::vector<std::pair<int, int> > validMoves() {
-    std::vector<std::pair<int, int> > pos;
-
-    // TODO check if squares are occupied
-    // first move can jump 2
-    if (color == WHITE) {
-      if (y == 1)
-	pos.push_back(std::make_pair(x, y + 2));
-      pos.push_back(std::make_pair(x, y + 1));
-    } else {
-      if (y == 6)
-	pos.push_back(std::make_pair(x, y - 2));
-      pos.push_back(std::make_pair(x, y - 1));
-    }
-
-    // TODO add diagonal capture movement
-    std::cout << "Pos: " << pos.size() << std::endl;
-    return pos;
-  }
+    Piece(color, x, y, "black_pawn.bmp", "white_pawn.bmp")
+  {}
 
   ~Pawn() {
     SDL_FreeSurface(surface);
   }
-
-private:
-  bool firstMove;
 };
 
 
